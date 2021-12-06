56b56f90f0be4f1f076feeb3a2634718ad21d233
eric <eric.thomas@formation-industries-fc.fr>
2021-12-06 16:40:45 +0100

    Exercice n°2

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+
+/***************************Exercice n°1******************************/
 int mesvaleurs[10];
+float tabnotes[30];
+int nbNotes=0;
 /*****************prototypes*************************/
 void saisie();
 void affichage();
+void saisieMoyenne();
+void moyenne();
 /*****************implementations********************/
 void saisie() {
     for(int i=0;i<10;i++)
@@ -19,8 +25,35 @@ void affichage() {
     }
 }
 
+void saisieMoyenne() {
+    for(int i=0;i<30;i++)
+    {
+        printf("Veuillez saisir une note\n");
+        scanf("%f",&tabnotes[i]);
+        nbNotes++;
+        moyenne(); //Appel moyenne
+    }
+}
+
+void moyenne() {
+    int somme=0;
+    for (int i=0;i<nbNotes;i++)
+    {
+        somme+=tabnotes[i]; // pour chaque case on incrémente la somme
+    }
+    printf("La moyenne des notes est de %0.2f\n",somme/(float)nbNotes);
+}
+
+
+/***********************Exercice n°2**********************************/
+
+
 int main() {
-   saisie();
-   affichage();
+   /******************Exercice n°1********************************/
+   //saisie();
+   //affichage();
+   /*****************Exercice n°2*********************************/
+   saisieMoyenne();
+   moyenne();
    return 0;
 }
